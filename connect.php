0e7cae3a4c84760e01d29a0cc1742934c856b97b
Epsa Sharma <99709007+20BCS4485@users.noreply.github.com>
2024-01-10 16:26:03 +0530

    round 2 code

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+    $signin = $_POST['signin'];
+    $check1 = $_POST['check1'];
+    $user = $_POST['user'];
+    $regemail = $_POST['regemail'];
+    $regpass = $_POST['regpass'];
+
+    
+
+?>
